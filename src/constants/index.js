ff09b75a871e5f89851c67326e5d96ce8e186317
Carlos Eduardo <cadu.010808@gmail.com>
2023-04-26 23:39:18 -0300

    finalizando a aplicação

--- src/constants/index.js
+++ src/constants/index.js
@@ -89,30 +89,14 @@ import {
       icon: tailwind,
     },
     {
-      name: "Node JS",
-      icon: nodejs,
-    },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
-      name: "Three JS",
-      icon: threejs,
-    },
-    {
       name: "git",
       icon: git,
     },
     {
       name: "figma",
       icon: figma,
-    },
-    {
-      name: "docker",
-      icon: docker,
-    },
-  ];
+    }
+  ]
   
   const experiences = [
     {
